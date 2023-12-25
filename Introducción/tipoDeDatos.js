d90268c4c360c9aaf854fb02a65897941479a70d
Luis <luis.riosbarrera@gmail.com>
2023-12-25 14:39:00 -0500

    Introduccion Parte I

---
+++ Introducción/tipoDeDatos.js
@@ -0,0 +1,36 @@
+// Cadenas de texto
+const nombre = 'Carlos';
+const parrafo = 'este es un "parrafo"';
+const parrafo2 = "este es un 'parrafo'";
+
+//Numeros
+const nuemro = 4;
+const numero2 = -4.527;
+
+//Booleanos
+const usuarioConectado = false
+const mayorQue = 10 > 2;
+console.log(mayorQue)
+
+//Arrays - Arreglos
+const arreglo = [1, 'texto', false, {propiedad: 'valor'}, [1.2]];
+console.log(arreglo)
+
+//Objetos
+const persona = {
+    nombre: 'Luis', 
+    apellido: 'Ríos',
+    edad: 17,
+    carro: {
+        marca: 'Chevrolet',
+        color: 'Cyan'
+    }
+};
+console.log(persona.carro.color)
+
+//Funciones
+function saludo() {
+    console.log('hola')
+}
+
+saludo()
---
+++ Introducción/variables.js
@@ -0,0 +1,18 @@
+var edad;
+let nombre = 'Luis';
+const correo = 'correo@gmail.com';
+console.log(correo)
+
+let telefono, pais, id;
+telefono = 123456789; 
+// console.log(telefono)
+
+const resultado = 4 + 4;
+console.log(resultado)
+const nombre1 = 'Carlos ';
+const nombre2 = 'Arturo';
+const nombreCompleto = nombre1 + nombre2;
+console.log(nombreCompleto);
+
+let miVariable = 'texto'
+miVariable = 7
